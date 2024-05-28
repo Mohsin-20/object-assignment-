c7464e7764159e5dd8e70213814384cb442279f8
Mohsin-20 <mohsinlakhani129@gmail.com>
2024-05-28 23:34:27 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,4 @@
+var miles = 130;
+var mile = 1.60934;
+var kilometer = miles * mile;
+console.log(kilometer);
---
+++ index.ts
@@ -0,0 +1,4 @@
+let miles: number = 130 ;
+let mile = 1.60934 ;
+let kilometer = miles*mile;
+console.log (kilometer);
